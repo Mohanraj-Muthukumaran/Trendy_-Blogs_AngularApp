abc4d20e6fa2b52ea6fb7d51895b7a825992a2a1
Mohanraj M <mohanrajmuthukumaran@gmail.com>
2020-10-14 16:18:04 +0530

    Trendy Blogs V 1.0 Updated

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+var forceSsl = require('force-ssl-heroku'); 
+
 const app = express();
- 
+
+app.use(forceSsl);
+
 // Serve only the static files form the angularapp directory
-app.use(express.static('https://'+__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
-    res.sendFile(path.join('https://'+__dirname+'/angularapp/index.html'));
+    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
