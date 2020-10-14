3f3bf17621f0a405c79887baaab524c471c4ef8e
Mohanraj M <mohanrajmuthukumaran@gmail.com>
2020-10-14 15:56:54 +0530

    Trendy Blogs V 1.0 Updated

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${path.join(__dirname)}+'/angularapp/index.html'`)
+      }
+    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
